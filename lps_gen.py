fd73b5b32895f54d026a904f8d3b60d69d32d28c
rsiddharth <rsd@gnu.org>
2015-12-12 21:01:45 -0500

    Added lps_gen.py + tests for it.
    Contains `LPSRenderer` and `LPSMarkdown` classes which convert a given
    Markdown LP schedule text to a python dictionary (OrderedDict).

---
+++ lps_gen.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2015  lpschedule-generator author(s). See AUTHORS.
+#
+#    This file is part of lpschedule-generator.
+#
+#   lpschedule-generator is free software: you can redistribute it
+#   and/or modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation, either version 3 of
+#   the License, or (at your option) any later version.
+#
+#   lpschedule-generator is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with lpschedule-generator (see COPYING).  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+from collections import OrderedDict
+from os import path
+
+from mistune import Renderer, Markdown
+
+# Python dictionary that will contain the lp schedule.
+lps_dict = OrderedDict()
+
+
+def read_file(filename):
+    """Read file and return it as a string.
+
+    filename: Absolute pathname of the file.
+    """
+    content = ''
+
+    with open(filename, 'rb') as f:
+        for line in f:
+            content = content + line
+
+    return content
+
+
+class LPSRenderer(Renderer):
+    """Helps in converting Markdown version of LP schedule to a dictionary.
+    """
+    def __init__(self, **kwargs):
+        super(LPSRenderer, self).__init__(**kwargs)
+        self.last_day = None
+        self.last_time_slot = None
+        self.last_session = None
+
+        # Denotes the no. of the paragraph under a session; this
+        # information will be helpful in identifying the "speaker",
+        # "room" and session "description".
+        self.no_paragraph = None
+
+
+    def header(self, text, level, raw=None):
+        global lps_dict
+
+        if level == 2:
+            # Add new day.
+            lps_dict[text] = OrderedDict()
+            self.last_day = text
+        elif level == 3:
+            # Add new timeslot
+            lps_dict[self.last_day][text] = OrderedDict()
+            self.last_time_slot = text
+        elif level == 4:
+            # Add new session
+            lps_dict[self.last_day][self.last_time_slot][text] = OrderedDict()
+            self.last_session = text
+            # We found a new session; set no of paragraphs processed
+            # to 0.
+            self.no_paragraph = 0
+
+        return super(LPSRenderer, self).header(text, level, raw)
+
+
+    def paragraph(self, text):
+        global lps_dict
+
+        p = super(LPSRenderer, self).paragraph(text)
+
+        if self.no_paragraph == 0:
+            # Speaker
+            if len(text.split(', ')) == 1:
+                speaker_text = text
+            else:
+                speaker_text = text.split(', ')
+
+            lps_dict[self.last_day][self.last_time_slot][
+                self.last_session]['speaker'] = speaker_text
+            self.no_paragraph = self.no_paragraph + 1
+        elif self.no_paragraph == 1:
+            # Room
+            lps_dict[self.last_day][self.last_time_slot][
+                self.last_session]['room'] = text
+            # Initialize description
+            lps_dict[self.last_day][self.last_time_slot][
+                self.last_session]['desc'] = []
+            self.no_paragraph = self.no_paragraph + 1
+        elif self.no_paragraph > 1:
+            lps_dict[self.last_day][self.last_time_slot][
+                self.last_session]['desc'].append(text)
+
+        return p
+
+
+class LPSMarkdown(Markdown):
+    """Converts MD LP schedule to a dictionary.
+
+    Returns the Markdown version of LP schedule as a dictionary.
+    """
+    def __init__(self, inline=None, block=None, **kwargs):
+        """
+        Initialize with LPSRenderer as the renderer.
+        """
+        super(LPSMarkdown, self).__init__(renderer=LPSRenderer(),
+                                          inline=None, block=None,
+                                          **kwargs)
+
+
+    def parse(self, text):
+        global lps_dict
+
+        lps_dict = OrderedDict()
+        html = super(LPSMarkdown, self).parse(text)
+        return lps_dict
---
+++ tests/test_lps_gen.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2015  lpschedule-generator author(s). See AUTHORS.
+#
+#    This file is part of lpschedule-generator.
+#
+#   lpschedule-generator is free software: you can redistribute it
+#   and/or modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation, either version 3 of
+#   the License, or (at your option) any later version.
+#
+#   lpschedule-generator is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with lpschedule-generator (see COPYING).  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+import json
+import pprint
+
+import mistune
+
+from os import path
+
+from nose.tools import *
+
+from lps_gen import *
+
+class TestLpsGen(object):
+    """
+    Class that tests the lps_gen.py module.
+    """
+    @classmethod
+    def setup_class(self):
+        """Runs before running any tests in this class."""
+
+        self.MD_FILE = path.join('tests', 'files', 'lp-sch.md')
+        self.MD_FILE_CONTENT = read_file(self.MD_FILE)
+
+        self.markdown = LPSMarkdown()
+        self.lps_dict = self.markdown(self.MD_FILE_CONTENT)
+
+    def setup(self):
+        """Runs before each test in this class."""
+        pass
+
+
+    def test_LPSMarkdown_day(self):
+        """
+        Testing `LPSMarkdown` class - Day.
+        """
+        days = ['Saturday, March 19',
+                'Sunday, March 20']
+        i = 0
+        for day in self.lps_dict.keys():
+            assert_equal(day, days[i])
+            i = i + 1
+
+
+    def test_LPSMarkdown_timeslot(self):
+        """
+        Testing `LPSMarkdown` class - Timeslot.
+        """
+        timeslots = [
+            '09:00 - 09:45: Registration and Breakfast',
+            '09:45 - 10:45: Opening Keynote: Richard Stallman',
+            '10:55 - 11:40: Session Block 1A',
+            '11:40 - 11:50: Break',
+            '11:50 - 12:35: Session Block 2A',
+            '09:00 - 09:45: Registration and breakfast',
+            '09:45 - 10:30: Keynote: Benjamin Mako Hill',
+            '10:30 - 10:40: Break',
+            '10:40 - 11:25: Session Block 1B',
+            ]
+
+        i = 0
+        for lps_timeslots in self.lps_dict.values():
+            for timeslot in lps_timeslots.keys():
+                assert_equal(timeslot, timeslots[i])
+                i = i + 1
+
+
+    def test_LPSMarkdown_session(self):
+        """
+        Testing `LPSMarkdown` class - Session.
+        """
+        sessions = [
+            'Free software, free hardware, and other things',
+            'Federation and GNU',
+            'Dr. Hyde and Mr. Jekyll: advocating for free software in nonfree academic contexts',
+            'TAFTA, CETA, TISA: traps and threats to Free Software Everywhere',
+            'Let\'s encrypt!',
+            'Attribution revolution -- turning copyright upside-down',
+            'Access without empowerment',
+            'Fork and ignore: fighting a GPL violation by coding instead',
+            'Who did this? Just wait until your father gets home',
+            ]
+
+        i = 0
+        for lps_timeslots in self.lps_dict.values():
+            for lps_sessions in lps_timeslots.values():
+                for session in lps_sessions.keys():
+                    assert_equal(session, sessions[i])
+                    i = i + 1
+
+
+    def test_LPSMarkdown_speaker(self):
+        """
+        Testing `LPSMarkdown` class - Speaker
+        """
+        speakers = [
+            'Richard Stallman',
+            'Christopher Webber',
+            'ginger coons',
+            ['Marianne Corvellec', 'Jonathan Le Lous'],
+            'Seth Schoen',
+            'Jonas Öberg',
+            'Benjamin Mako Hill',
+            'Bradley Kuhn',
+            'Ken Starks',
+            ]
+
+        i = 0
+        for lps_timeslots in self.lps_dict.values():
+            for lps_sessions in lps_timeslots.values():
+                for session_info in lps_sessions.values():
+                    assert_equal(session_info['speaker'], speakers[i])
+                    i = i + 1
+
+
+    def test_LPSMarkdown_room(self):
+        """
+        Testing `LPSMarkdown` class - Room
+        """
+        rooms = [
+            'Room 32-123',
+            'Room 32-123',
+            'Room 32-141',
+            'Room 32-155',
+            'Room 32-123',
+            'Room 32-141',
+            'Room 32-123',
+            'Room 32-123',
+            'Room 32-141',
+            ]
+        i = 0
+        for lps_timeslots in self.lps_dict.values():
+            for lps_sessions in lps_timeslots.values():
+                for session_info in lps_sessions.values():
+                    assert_equal(session_info['room'], rooms[i])
+                    i = i + 1
+
+
+    def teardown(self):
+        """Cleans up things after each test in this class."""
+        pass
+
+
+    @classmethod
+    def teardown_class(self):
+        """Purge the mess created by this test."""
+        pass
